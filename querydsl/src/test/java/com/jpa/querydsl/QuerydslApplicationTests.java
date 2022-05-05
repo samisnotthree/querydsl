331b15e00fe93db4faddcaeda5cce15ccb6aaf8d
samisnotthree <samisnotthree@gmail.com>
2022-05-05 21:31:09 +0900

    환경설정 및 조회

--- querydsl/src/test/java/com/jpa/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/com/jpa/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package com.jpa.querydsl;
 
+import com.jpa.querydsl.entity.Hello;
+import com.jpa.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
